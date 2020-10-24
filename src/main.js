80345f79a9197d7a6ba411bed95c7a6ee16ebb7e
iiiihua <iiiihua@163.com>
2020-10-25 00:51:17 +0800

    上线测试

--- src/main.js
+++ src/main.js
@@ -14,9 +14,17 @@ import VueQuillEditor from 'vue-quill-editor'
 import 'quill/dist/quill.core.css' // import styles
 import 'quill/dist/quill.snow.css' // for snow theme
 import 'quill/dist/quill.bubble.css' // for bubble theme
+import Nprogress from 'nprogress'
+import 'nprogress/nprogress.css'
 axios.defaults.baseURL = 'http://118.25.44.48:8888/api/private/v1/'
+
 axios.interceptors.request.use(config => {
+  Nprogress.start()
   config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+axios.interceptors.response.use(config => {
+  Nprogress.done()
   return config
 })
 Vue.prototype.$http = axios
