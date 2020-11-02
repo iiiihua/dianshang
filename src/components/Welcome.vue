e2c4d78db5bdb24a50ab8f678a28bb036c1a7ff5
iiiihua <iiiihua@163.com>
2020-11-02 16:26:24 +0800

    优化

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
-      <h3>Welcome to Free-Shop后台管理</h3>
-      <a href="http://www.testall.top">欢迎访问我的个人博客</a>
+      <h2>Welcome to Free-Shop后台管理</h2>
+      <h4>此项目使用<a href="https://cn.vuejs.org/">vue2</a>和<a href="https://element.eleme.cn/#/zh-CN">ElementUi</a>开发</h4>
+      <h4>后台服务运行在<a href="https://cloud.tencent.com/">腾讯云</a>服务器</h4>
+      欢迎访问我的<a href="http://www.testall.top">个人博客</a>和<a href="https://github.com/iiiihua">Github</a>
     </div>
 </template>
 
@@ -12,5 +14,9 @@ export default {
 </script>
 
 <style scoped>
+  a{
+    text-decoration: none;
+    color: #ff9900;
+  }
 
 </style>
